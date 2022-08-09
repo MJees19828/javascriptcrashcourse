81639342e22334f9202091a70788a0838d02c567
MJees19828 <101269780+MJees19828@users.noreply.github.com>
2022-08-09 17:09:35 +0000

    working with external js file

---
+++ main.js
@@ -0,0 +1,20 @@
+if (3==3 && 4==4){
+  document.write("the condition is true")
+}
+if (3==3 && 4==4){
+  document.write("the condition is false and it will not be printed")
+}
+const age = 21;
+if(age>18){
+  alert("old enough to vote")
+}else{
+  alert("Not old enough to register")
+}
+let x = 10;
+if(x<10){
+  document.getElementById("demo ").innerHTML= "X is less than 10";
+}else if(x>10){
+  document.getElementById("demo").innerHTML= "X is greater than 10";
+}else{
+  document.getElementById("demo").innerHTML= "X is equals to 10";
+}
